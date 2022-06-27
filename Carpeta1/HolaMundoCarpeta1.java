8d7747bec4ddd2efeba00ae1806ebaecdef23e40
luis <luiscr777@gmail.com>
2022-06-27 13:19:38 -0500

    Modifiqué en HolaMundoCarpeta1

--- Carpeta1/HolaMundoCarpeta1.java
+++ Carpeta1/HolaMundoCarpeta1.java
@@ -7,6 +7,7 @@ public class HolaMundoCarpeta1 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Realicé un cambio al código en Carpeta 1");
+        System.out.println("Realicé un cambio más al código en Carpeta 1");
     }
     
 }
