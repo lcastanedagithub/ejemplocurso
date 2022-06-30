0fb8ef700999504ecbac289ebe66323268d63784
luis <luiscr777@gmail.com>
2022-06-30 14:02:56 -0500

    Antes de hacer merge

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,17 +1,11 @@
-
 package prueba;
-
-
 public class HolaMundo {
-
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Realicé un cambio al código");
         System.out.println("Realicé otro cambio al código");
-        System.out.println("Realice otro cambio desde Github");
-        
+        System.out.println("Realice otro cambio desde Github");        
         System.out.println("Este cambio es en el Branch prueba");
-        
     }
-    
+    //Esta linea presentará conflicto
 }
