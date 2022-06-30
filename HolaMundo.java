5353f23abbc01fbe22b35f990e2590be9e962441
luis <luiscr777@gmail.com>
2022-06-30 14:04:44 -0500

    Antes de hacer merge desde master

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,18 +1,13 @@
-
 package prueba;
-
-
 public class HolaMundo {
-
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Realicé un cambio al código");
         System.out.println("Realicé otro cambio al código");
-        System.out.println("Realice otro cambio desde Github");
-        
+        System.out.println("Realice otro cambio desde Github");        
         System.out.println("Agregué este codigo antes de hacer el Branch");
         System.out.println("el branch a crear será prueba");
-        
+        //Que va pasar en esta línea
     }
     
 }
